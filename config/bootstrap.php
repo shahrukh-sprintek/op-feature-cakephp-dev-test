abed08e5b1f407961762d60e01226c21d4ac49a5
zeeshanalisprintech <zeeshan.ali@sprintech.digital>
2021-01-26 21:30:42 +0500

    cors header is added

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -201,3 +201,10 @@ Type::build('timestamp')
 //Inflector::rules('irregular', ['red' => 'redlings']);
 //Inflector::rules('uninflected', ['dontinflectme']);
 //Inflector::rules('transliteration', ['/å/' => 'aa']);
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, PUT, PATCH, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: *');
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit(0);
+}
